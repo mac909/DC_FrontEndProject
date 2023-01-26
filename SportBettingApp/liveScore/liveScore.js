9cc505c12ad4be59e31207ad4aec796d4a5ce04d
Twalib <twalib.tmo@gmail.com>
2023-01-25 23:16:46 -0500

    new work on creating table and javascript api callings

--- SportBettingApp/liveScore/liveScore.js
+++ SportBettingApp/liveScore/liveScore.js
@@ -1,16 +1,63 @@
 
+// const options = {
+//     method: 'GET',
+//     url: 'https://odds.p.rapidapi.com/v4/sports/basketball_nba/scores',
+//     params: {daysFrom: '1'},
+//     headers: {
+//       'X-RapidAPI-Key': 'c2e100179emshee202f59bef416fp1d6359jsn6c971475cebe',
+//       'X-RapidAPI-Host': 'odds.p.rapidapi.com'
+//     }
+//   };
+
+// fetch('https://odds.p.rapidapi.com/v4/sports/basketball_nba/scores', options)
+// 	.then(response => response.json())
+// 	.then(response => console.log(response));
+
+
+// // you need to write a fuction which takes date as a param and returns a weekday.
+
+
+//     // will i need to loop around different days to showcase the data collected?
 const options = {
     method: 'GET',
     url: 'https://odds.p.rapidapi.com/v4/sports/basketball_nba/scores',
-    params: {daysFrom: '3'},
+    params: {daysFrom: '1'},
     headers: {
-      'X-RapidAPI-Key': 'c2e100179emshee202f59bef416fp1d6359jsn6c971475cebe',
-      'X-RapidAPI-Host': 'odds.p.rapidapi.com'
+    'X-RapidAPI-Key': 'c2e100179emshee202f59bef416fp1d6359jsn6c971475cebe',
+    'X-RapidAPI-Host': 'odds.p.rapidapi.com'
     }
-  };
+};
 
 fetch('https://odds.p.rapidapi.com/v4/sports/basketball_nba/scores', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err))
-    
+    .then(response => response.json())
+    .then((response) => {
+        console.log(response);
+        //console.log("inside--------------------/n" + response[0].home_team);
+        // document.getElementById("mon").innerHTML = response[0].home_team;
+
+        // we going to need a fuction which loops through the json object
+        //  and put data into the table data
+        // we need another function to change date to days.
+        console.log("has to give us days" + getDayOfWeek(response[0].last_update));
+        for (var key in response) {
+            if(0 === 0) {
+                console.log(key);
+            } 
+        }
+
+
+    });
+
+    function getDayOfWeek(date) {
+            const dayOfWeek = new Date(date).getDay();    
+            return isNaN(dayOfWeek) ? null : 
+                ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][dayOfWeek];
+            }
+
+
+
+
+    // getDays();
+    // function getDays() {
+    //     document.getElementById("mon").innerHTML = "this is monday";
+    // } 
