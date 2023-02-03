bac9495798a06383b5d6dd147f2bdadf69e67fbe
Mason Mccabe <mason.mccabe2015@gmail.com>
2023-02-03 11:02:59 -0500

    Submit btn fix to go to homepage

--- SportBettingApp/app.js
+++ SportBettingApp/app.js
@@ -1,19 +1,19 @@
 // To redirect the login page to the homepage.
-const reDirect = (document.getElementById("buttonclick").onclick = () => {
-  location.href = "homepage/homepage.html";
-});
 
 // To change the text of the button from submit to LogIn.
-document.querySelector("#buttonclick").value = "LogIn";
+// document.querySelector("#buttonclick").value = "LogIn";
 
 // To remove the disabled button just fill out both username and password text bars.
 input = () => {
   let button = document.getElementById("buttonclick");
+  let link = document.getElementById("linkHome");
   let text = document.getElementById("bar1");
   let password = document.getElementById("bar2");
   if (text.value && password.value != "") {
     button.disabled = false;
+    link.setAttribute("style", "'pointer-events: all;'");
   } else {
     button.disabled = true;
+    link.setAttribute("style", "'pointer-events: none;'");
   }
 };
