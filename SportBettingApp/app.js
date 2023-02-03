1841a6da51b4f9dfc042c40b29637a84d0198b91
jmeza777 <super0268@gmail.com>
2023-02-03 08:43:16 -0800

    fixed onclick function

--- SportBettingApp/app.js
+++ SportBettingApp/app.js
@@ -1,11 +1,14 @@
 // To redirect the login page to the homepage.
-
+const reDirect = document.getElementById("buttonClick").onclick = () => {
+  location.href = "homepage/homepage.html"
+  return false;
+}
 // To change the text of the button from submit to LogIn.
 // document.querySelector("#buttonclick").value = "LogIn";
 
 // To remove the disabled button just fill out both username and password text bars.
 input = () => {
-  let button = document.getElementById("buttonclick");
+  let button = document.getElementById("buttonClick");
   let link = document.getElementById("linkHome");
   let text = document.getElementById("bar1");
   let password = document.getElementById("bar2");
